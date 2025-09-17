80c1234bfd3f215a87d269706b7fd533e4a309b8
Juan-Riverax <juanjisx2@gmail.com>
2025-09-17 11:18:45 -0500

    Add files via upload

---
+++ Corte1/C/Practica2/Condicional2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int numero = -5;
+	if (numero > 0) {
+		printf("Su numero es positivo.\n");
+	} else {
+		printf("Su numero es negativo.\n");
+	}
+	return 0;
+}
